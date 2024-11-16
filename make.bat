d3751c83a36a0b5a9e3182b8b789bfc11da390da
sadam-04 <asaich3@yahoo.com>
2024-11-16 18:30:34 -0500

    amend debug-prod PR

--- make.bat
+++ make.bat
@@ -1,10 +1,11 @@
 @ECHO OFF
 mkdir out
-if %1==debug (
+if "%1"=="debug" (
     echo Compiling debug version...
     cl src/main.cpp src/nayuki-qr/qrcodegen.cpp src/webserver.cpp src/dataloader.cpp src/qr_to_bmp.cpp src/window.cpp /Fe:out/Axon.exe /Fo:out/ /I src/SFML-2.6.2/include /link /LIBPATH:"src\SFML-2.6.2\lib" sfml-graphics.lib sfml-window.lib sfml-system.lib
 
-) else (
+)
+else (
     echo Compiling prod version...
     cl src/main.cpp src/nayuki-qr/qrcodegen.cpp src/webserver.cpp src/dataloader.cpp src/qr_to_bmp.cpp src/window.cpp /Fe:out/Axon.exe /Fo:out/ /I src/SFML-2.6.2/include /link /SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup /LIBPATH:"src\SFML-2.6.2\lib" sfml-graphics.lib sfml-window.lib sfml-system.lib
 )
