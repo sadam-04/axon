a3ebf3805b0d135118355fa765a48380d37f038b
sadam-04 <asaich3@yahoo.com>
2024-11-16 18:34:42 -0500

    amendment 2 to debug-prod PR

--- make.bat
+++ make.bat
@@ -2,10 +2,10 @@
 mkdir out
 if "%1"=="debug" (
     echo Compiling debug version...
-    cl src/main.cpp src/nayuki-qr/qrcodegen.cpp src/webserver.cpp src/dataloader.cpp src/qr_to_bmp.cpp src/window.cpp /Fe:out/Axon.exe /Fo:out/ /I src/SFML-2.6.2/include /link /LIBPATH:"src\SFML-2.6.2\lib" sfml-graphics.lib sfml-window.lib sfml-system.lib
-
-)
-else (
+    mkdir "out/debug/"
+    cl src/main.cpp src/nayuki-qr/qrcodegen.cpp src/webserver.cpp src/dataloader.cpp src/qr_to_bmp.cpp src/window.cpp /Fe:out/debug/Axon.exe /Fo:out/debug/ /I src/SFML-2.6.2/include /link /LIBPATH:"src\SFML-2.6.2\lib" sfml-graphics.lib sfml-window.lib sfml-system.lib
+) else (
     echo Compiling prod version...
-    cl src/main.cpp src/nayuki-qr/qrcodegen.cpp src/webserver.cpp src/dataloader.cpp src/qr_to_bmp.cpp src/window.cpp /Fe:out/Axon.exe /Fo:out/ /I src/SFML-2.6.2/include /link /SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup /LIBPATH:"src\SFML-2.6.2\lib" sfml-graphics.lib sfml-window.lib sfml-system.lib
+    mkdir "out/prod/"
+    cl src/main.cpp src/nayuki-qr/qrcodegen.cpp src/webserver.cpp src/dataloader.cpp src/qr_to_bmp.cpp src/window.cpp /Fe:out/prod/Axon.exe /Fo:out/prod/ /I src/SFML-2.6.2/include /link /SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup /LIBPATH:"src\SFML-2.6.2\lib" sfml-graphics.lib sfml-window.lib sfml-system.lib
 )
