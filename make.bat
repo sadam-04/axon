2d344012935a791414558037892e0672906bb811
sadam-04 <asaich3@yahoo.com>
2024-11-16 01:01:00 -0500

    add output mkdir to makefile

--- make.bat
+++ make.bat
@@ -1,2 +1,3 @@
 @ECHO OFF
+mkdir out
 cl src/main.cpp src/nayuki-qr/qrcodegen.cpp src/webserver.cpp src/dataloader.cpp src/qr_to_bmp.cpp src/window.cpp /Fe:out/flyle.exe /Fo:out/ /I src/SFML-2.6.2/include /link /SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup /LIBPATH:"src\SFML-2.6.2\lib" sfml-graphics.lib sfml-window.lib sfml-system.lib
