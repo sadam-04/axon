5b9616e56f8483679e3a4b299c864d3c94c4868a
sadam-04 <asaich3@yahoo.com>
2024-11-19 15:46:10 -0500

    webserver overhaul

--- src/webserver.cpp
+++ src/webserver.cpp
@@ -7,7 +7,7 @@
 
 #pragma comment(lib, "Ws2_32.lib")
 
-std::string shortenPath(std::string path)
+std::string getFilename(std::string path)
 {
     while (path.find('/') != std::string::npos)
         path.erase(0, 1);
@@ -41,72 +41,43 @@ std::string getFileExt(std::string fname)
     return fname;
 }
 
-void handleClient(SOCKET clientSocket, std::string filename, std::string &data, std::string url_path, std::string mimetype, std::string sug_cont_dispo) { //suggested content disposition
+void handleClient(SOCKET clientSocket, std::string correct_path, std::string &http_200_response, std::string &http_404_response) { //suggested content disposition
     // Buffer for receiving data
     char buffer[1024] = {0};
     int bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-    
     if (bytesReceived == SOCKET_ERROR) {
         std::cerr << "Receive failed with error: " << WSAGetLastError() << std::endl;
         closesocket(clientSocket);
         return;
     }
-
     buffer[bytesReceived] = '\0';
 
-    // Simple HTTP response
-    std::string response_body =
-        "<!DOCTYPE html><html><head><title>For, oh fore</title></head>"
-        "<body><h1>nothing here</h1></body></html>";
-
-    std::string cont_dispo = "inline";
-
-    std::string reqPath = parseRequestPath(buffer);
+    //get the requested url path
+    std::string request_path = parseRequestPath(buffer);
     
-    
-    if (reqPath == std::string(url_path))
+    const char *http_response = http_404_response.c_str();
+    unsigned int http_response_size = http_404_response.size();
+
+    //if requested path matches the correct one, send 200 response, otherwise send 404.
+    if (request_path == std::string(correct_path))
     {
-        //response_type = "application/octet-stream";
-        response_body = data;
-        cont_dispo = sug_cont_dispo;
+        http_response = http_200_response.c_str();
+        http_response_size = http_200_response.size();
     }
-        
-
-    std::string httpResponse =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: " + mimetype + "\r\n"
-        "Content-Length: " + std::to_string(response_body.size()) + "\r\n"
-        + "Content-Disposition: " + cont_dispo + (cont_dispo=="attachment" ? ("; filename="+shortenPath(filename)+"\r\n") : (""))
-        + "Connection: close\r\n\r\n"
-        + response_body;
 
     // Send the response to the client
-    send(clientSocket, httpResponse.c_str(), httpResponse.size(), 0);
+    send(clientSocket, http_response, http_response_size, 0);
 
     // Close the client socket
     closesocket(clientSocket);
 }
 
-int webserver(std::string filename, std::string &data, unsigned int port, std::string url_path) {
+int webserver(std::string filepath, std::string &data, unsigned int port, std::string url_path) {
     WSADATA wsaData;
     SOCKET serverSocket, clientSocket;
     struct sockaddr_in serverAddr, clientAddr;
     int clientAddrLen = sizeof(clientAddr);
 
-    std::string ext, content_disposition = "attachment", mimetype = "application/octet-stream";
-    ext = getFileExt(filename);
-
-    if (ext == "jpg" || ext == "jpeg")
-        mimetype = "image/jpeg";
-    else if (ext == "png")
-        mimetype = "image/png";
-    else if (ext == "pdf")
-        mimetype = "application/pdf";
-    else if (ext == "mp4")
-    {
-        mimetype = "video/mp4";
-        content_disposition = "inline";
-    }
     // Initialize Winsock
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
         std::cerr << "WSAStartup failed with error: " << WSAGetLastError() << std::endl;
@@ -142,9 +113,61 @@ int webserver(std::string filename, std::string &data, unsigned int port, std::s
         return 1;
     }
 
-    std::vector<std::thread> responses;
+
+
+
+
+
+
+
+    //build http_200_response
+    std::string ext, content_disposition = "attachment", mimetype = "application/octet-stream";
+    ext = getFileExt(getFilename(filepath));
+
+    std::vector<std::pair<std::vector<std::string>, std::string>> known_types = {
+        {{"jpg", "jpeg"}, "image/jpeg"},
+        {{"png"}, "image/png"},
+        {{"pdf"}, "application/pdf"},
+        {{"mp4"}, "video/mp4"},
+        {{"txt"}, "text/plain"}
+    };
+
+    for (int t = 0; t < known_types.size(); t++)
+    {
+        for (int e = 0; e < known_types[t].first.size(); e++)
+        {
+            if (ext == known_types[t].first[e]) mimetype = known_types[t].second;
+        }
+    }
+
+    std::string http_200_response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: " + mimetype + "\r\n"
+        "Content-Length: " + std::to_string(data.size()) + "\r\n"
+        //+ "Content-Disposition: " + content_disposition + (content_disposition=="attachment" ? ("; filename=\""+getFilename(filepath)+"\"") : ("")) + "\r\n"
+        + "Content-Disposition: " + content_disposition + (content_disposition=="attachment" ? ("; filename=\""+getFilename(filepath)+"\"") : ("")) + "\r\n"
+        + "Connection: close\r\n\r\n"
+        + data;
+
+    // Simple HTTP response
+    std::string body_404 =
+        "<!DOCTYPE html><html><head><title>Fore, oh fore</title></head>"
+        "<body><h1>404 Not Found</h1>\n<p>check your url path or scan the QR code</p></body></html>";
+    std::string http_404_response =
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "Content-Length: " + std::to_string(body_404.size()) + "\r\n"
+        + "Content-Disposition: inline\r\n"
+        + "Connection: close\r\n\r\n"
+        + body_404;
+
+
+
+
+
 
     // Main server loop
+    std::vector<std::thread> responses;
     while (true) {
         // Accept a client socket
         clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
@@ -154,7 +177,7 @@ int webserver(std::string filename, std::string &data, unsigned int port, std::s
         }
 
         // Handle the client request
-        responses.push_back(std::thread(handleClient, clientSocket, filename, std::ref(data), url_path, mimetype, content_disposition));
+        responses.push_back(std::thread(handleClient, clientSocket, url_path, std::ref(http_200_response), std::ref(http_404_response)));
     }
 
     // Cleanup
