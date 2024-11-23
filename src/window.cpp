48be8ecddefa77793abf259b31ced452942b0b80
sadam-04 <asaich3@yahoo.com>
2024-11-23 04:26:15 -0500

    functional receive

--- src/window.cpp
+++ src/window.cpp
@@ -57,9 +57,13 @@ int createWindow(std::string qr_bmp_send, std::string qr_bmp_recv, std::string f
     window.setIcon(ICON_WIDTH, ICON_HEIGHT, ICON_RGBA);
 
     // Load the BMP image into a texture
-    sf::Texture texture;
-    if (!texture.loadFromMemory(qr_bmp_send.c_str(), qr_bmp_send.size())) {
-        std::cerr << "Error: Could not load image file!" << std::endl;
+    sf::Texture qr_send_tex, qr_recv_tex;
+    if (!qr_send_tex.loadFromMemory(qr_bmp_send.c_str(), qr_bmp_send.size())) {
+        std::cerr << "Error: Could not load send qr BMP file!" << std::endl;
+        return 1;
+    }
+    if (!qr_recv_tex.loadFromMemory(qr_bmp_recv.c_str(), qr_bmp_recv.size())) {
+        std::cerr << "Error: Could not load recv qr BMP file!" << std::endl;
         return 1;
     }
 
@@ -85,27 +89,39 @@ int createWindow(std::string qr_bmp_send, std::string qr_bmp_recv, std::string f
     //text.setStyle(sf::Text::Bold | sf::Text::Underlined);
 
     // Create a sprite to display the texture
-    sf::Sprite qrSpr;
-    qrSpr.setTexture(texture);
+    sf::Sprite qr_spr_send, qr_spr_recv;
+    qr_spr_send.setTexture(qr_send_tex);
+    qr_spr_recv.setTexture(qr_recv_tex);
 
     //sf::RectangleShape qrBG(sf::Vector2f(325.f, 300.f));
 
-    qrSpr.setScale(12.f, 12.f);
+    qr_spr_send.setScale(12.f, 12.f);
+    qr_spr_recv.setScale(12.f, 12.f);
     //qrBG.setFillColor(sf::Color::Black);
 
-    centerSprite(qrSpr, window);
+    centerSprite(qr_spr_send, window);
+    centerSprite(qr_spr_recv, window);
     centerSprite(url_text, window);
     centerSprite(fn_text, window);
     //centerSprite(qrBG, window);
 
-    url_text.setPosition(url_text.getPosition().x, qrSpr.getGlobalBounds().top + qrSpr.getGlobalBounds().height + 6.f);
-    fn_text.setPosition(fn_text.getPosition().x, qrSpr.getGlobalBounds().top - fn_text.getLocalBounds().height - 10.f);
+    url_text.setPosition(url_text.getPosition().x, qr_spr_send.getGlobalBounds().top + qr_spr_send.getGlobalBounds().height + 6.f);
+    fn_text.setPosition(fn_text.getPosition().x, qr_spr_send.getGlobalBounds().top - fn_text.getLocalBounds().height - 10.f);
+
+    sf::Sprite *current_qr = &qr_spr_send;
 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::KeyPressed)
+                if (event.key.scancode == sf::Keyboard::Scan::Space)
+                {
+                    if (current_qr == &qr_spr_send) current_qr = &qr_spr_recv; else current_qr = &qr_spr_send;
+                    if (fn_text.getString() == filename) fn_text.setString("Receive"); else fn_text.setString(filename);
+                    if (url_text.getString() == url_send) url_text.setString(url_recv); else url_text.setString(url_send);
+                }
         }
 
         // Clear, draw, and display
@@ -113,7 +129,7 @@ int createWindow(std::string qr_bmp_send, std::string qr_bmp_recv, std::string f
         window.draw(fn_text);
         window.draw(url_text);
         //window.draw(qrBG);
-        window.draw(qrSpr);
+        window.draw(*current_qr);
         window.display();
     }
 
